998b0e5a5a3aba9d487d5d7381dd1e69a6f46520
rudreshsolank97 <rudreshsolanki101@gmail.com>
2020-01-24 01:51:07 +0530

    upto basic setup of custom-page

---
+++ tests/factories/sessionFactory.js
@@ -0,0 +1,15 @@
+const Buffer = require('safe-buffer').Buffer;
+const Keygrip = require('keygrip');
+const keys = require('../../config/keys');
+
+module.exports = user => {
+    const sessionObj = {
+        passport: {
+            user: user._id.toString()
+        }
+    };
+    const session = Buffer.from(JSON.stringify(sessionObj)).toString('base64');
+    const keygrip = Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + session);
+    return { session, sig };
+};
--- tests/header.test.js
+++ tests/header.test.js
@@ -1,20 +1,20 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
+let Page = require('./helpers/page');
 
-let browser, page;
+let page;
 
 beforeEach(async() => {
-    browser = await puppeteer.launch({
-        headless: false
-    });
-    page = await browser.newPage();
+    page = await Page.build();
     await page.goto('http://localhost:3000');
 });
 
 afterEach(async() => {
-    await browser.close();
+    await page.close();
 });
 
-test('We laucnha puppeteer browser', async() => {
+test('We launch puppeteer browser', async() => {
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster');
 });
@@ -24,4 +24,10 @@ test('clicking login starts the oauth flow', async() => {
     const url = await page.url();
 
     expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('when signed in, shows logout button', async() => {
+    await page.login();
+    const text = await page.getContentsOf('a[href="/auth/logout"]');
+    expect(text).toEqual('Logout');
 });
