976876371fa037b499d889a7bb10f2a223cf1b8a
rudreshsolank97 <rudreshsolanki101@gmail.com>
2020-01-25 03:02:02 +0530

    completed unit testing #120

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -4,12 +4,16 @@ const userFactory = require('../factories/userFactory');
 
 class CustomPage {
     static async build() {
-        const browser = await puppeteer.launch({ headless: false });
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox']
+        });
+
         const page = await browser.newPage();
         const customPage = new CustomPage(page);
 
         return new Proxy(customPage, {
-            get: (target, property) => {
+            get: function(target, property) {
                 return customPage[property] || browser[property] || page[property];
             }
         });
@@ -25,12 +29,49 @@ class CustomPage {
 
         await this.page.setCookie({ name: 'session', value: session });
         await this.page.setCookie({ name: 'session.sig', value: sig });
-        await this.page.goto('localhost:3000');
+        await this.page.goto('http://localhost:3000/blogs');
         await this.page.waitFor('a[href="/auth/logout"]');
     }
 
     async getContentsOf(selector) {
         return this.page.$eval(selector, el => el.innerHTML);
+    }
+
+    get(path) {
+        return this.page.evaluate(_path => {
+            return fetch(_path, {
+                method: 'GET',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json());
+        }, path);
+    }
+
+    post(path, data) {
+        return this.page.evaluate(
+            (_path, _data) => {
+                return fetch(_path, {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(_data)
+                }).then(res => res.json());
+            },
+            path,
+            data
+        );
+    }
+
+    execRequests(actions) {
+        return Promise.all(
+            actions.map(({ method, path, data }) => {
+                return this[method](path, data);
+            })
+        );
     }
 }
 
