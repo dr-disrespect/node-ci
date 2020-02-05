8332b644b694401cee1418d690c929a8f7c08158
rudreshsolank97 <rudreshsolanki101@gmail.com>
2020-02-06 01:30:39 +0530

    travis config (4)

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     "933479857663-lna1gjua0a06lojpsqt0qco7kvrmu6mf.apps.googleusercontent.com",
   googleClientSecret: "ZwafpwktNcVsQcc2CzziSBrS",
-  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
+  mongoURI: "mongodb://localhost/blog_ci",
   cookieKey: "asdasdasd",
   redisUrl: "redis://127.0.0.1:6379"
 };
